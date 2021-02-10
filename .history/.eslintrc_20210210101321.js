01ec67331192c9a29ae7ed64a75d1db2c6ea80e2
Milenko <milenko.markovic@gmail.com>
2021-02-10 10:34:04 +0100

    add pages

---
+++ .history/.eslintrc_20210210101321.js
@@ -0,0 +1,40 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2020": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended'
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+                // I will use TypeScript's types for component props instead
+                'react/prop-types': 'off',
+
+                // No need to import React when using Next.js
+                'react/react-in-jsx-scope': 'off',
+        
+                // This rule is not compatible with Next.js's <Link /> components
+                'jsx-a11y/anchor-is-valid': 'off',
+        
+                // Why would you want unused vars?
+                '@typescript-eslint/no-unused-vars': ['error'],
+
+                
+  },
+};
